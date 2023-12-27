76b27e4f9a2f92d6789d10568784d8e22bc60642
ashleyann9535 <65263262+ashleyann9535@users.noreply.github.com>
2023-12-26 20:46:38 -0600

    clean up app and add navbar

---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div className="Navbar">
+            <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">
+                    <img src="/tennis-ball.png" alt="" width="30" height="24" class="d-inline-block align-text-top"></img>
+                     Tennis App
+                </a>
+            </div>
+        </nav>
+        </div>
+    );
+  }
+
+export default Navbar;
